da09cae003e1943f7d96c7306626edfc906a8d7a
Charles Hsieh <charlous167@gmail.com>
2021-04-17 01:47:44 +0800

    Create Xcode project and .gitignore file

---
+++ KDCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KDCalculator
+//
+//  Created by Charles Hsieh on 2021/4/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
